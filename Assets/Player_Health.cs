bf67a27426fd58d67ba3c9ee5bec623f18589110
Jonah Stockdill <jonahstockdill@MacBook-Pro-1199.local>
2018-08-20 22:24:30 -0400

    #1 add camera and death

---
+++ Assets/Player_Health.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Health : MonoBehaviour {
+
+
+    public int health;
+    public bool hasDied;
+
+	// Use this for initialization
+	void Start () {
+        hasDied = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.transform.position.y < -7) {
+            hasDied = true;
+        }
+
+        if (hasDied == true) {
+            StartCoroutine("Die");
+            
+        }
+		
+	}
+
+    IEnumerator Die () {
+        Debug.Log("Player has fallen");
+        yield return new WaitForSeconds(2);
+
+        Debug.Log("Player has died");
+        SceneManager.LoadScene("wip_1");
+        yield return null;
+    }
+
+}
+
